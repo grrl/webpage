b20c9b021b8561b0837db29e5b963080cc0c01e3
Jonesジョーンズ <jooj@uef.fi>
2022-01-04 16:00:23 +0200

    uwu

--- book.js
+++ book.js
@@ -1,3 +1,23 @@
 // JavaScript source code
 
 //activate when user presses submi
+
+/*
+
+@book{hawking1989,
+  title     = "A Brief History of Time: From the Big Bang to Black Holes",
+  author    = "Hawking, Stephen",
+  year      = 1988,
+  publisher = "Bantam",
+  address   = "London"
+}
+ 
+*/
+
+function book_function() {
+
+    var code = "";
+    var title = "title = " + '"' ;
+
+
+}
