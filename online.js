89644ee61bdcd17cd6cf53f0570f71f21798eb5d
user <user@DESKTOP-09J267L>
2022-04-02 15:29:45 +0300

    uwu

--- online.js
+++ online.js
@@ -1,1 +1,63 @@
 // JavaScript source code
+
+
+var keywords_array = [];
+
+//struct?
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function book_function() {
+
+    var book_string = "@book{";
+    var code = document.getElementById("code").value + ',\n';
+    var title = '  title     = "' + document.getElementById("title").value + '",\n';
+    var author = '  author    = "' + document.getElementById("title").value + '",\n';
+    var year = '  year      = ' + document.getElementById("year").value + ',\n';
+    var publisher = '  publisher = "' + document.getElementById("publisher").value + '",\n';
+    var address = '  address   = "' + document.getElementById("address").value + '"\n' + "}";
+
+    book_string += code;
+    book_string += title;
+    book_string += author;
+    book_string += year;
+    book_string += publisher;
+    book_string += address;
+    navigator.clipboard.writeText(book_string);
+}
+
+function book_clear() {
+
+    if (document.getElementById("book_button_clear").innerHTML == "Confirm") {
+
+        document.getElementById("code").value = "";
+        document.getElementById("title").value = "";
+        document.getElementById("title").value = "";
+        document.getElementById("year").value = "";
+        document.getElementById("publisher").value = "";
+        document.getElementById("address").value = "";
+        document.getElementById("book_button_clear").style.background = '#087cfc';
+        document.getElementById("book_button_clear").style.borderColor = '#087cfc';
+        document.getElementById("book_button_clear").innerHTML = "Clear all";
+    }
+    else {
+
+        document.getElementById("book_button_clear").style.background = '#7CFC00';
+        document.getElementById("book_button_clear").style.borderColor = '#7CFC00';
+        document.getElementById("book_button_clear").innerHTML = "Confirm"
+        //document.getElementById("article_button_clear").id = "confirm_button"
+
+        sleep(5000).then(() => {
+
+            if (document.getElementById("book_button_clear").innerHTML == "Confirm") {
+                document.getElementById("book_button_clear").style.background = '#087cfc';
+                document.getElementById("book_button_clear").style.borderColor = '#087cfc';
+                document.getElementById("book_button_clear").innerHTML = "Clear all"
+            }
+
+        });
+    }
+
+}
