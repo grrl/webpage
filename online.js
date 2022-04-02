6a7cbbbbaf6416d8046039233e4c46895558caf9
user <user@DESKTOP-09J267L>
2022-04-02 16:58:50 +0300

    uwu

--- online.js
+++ online.js
@@ -11,13 +11,13 @@ function sleep(time) {
 
 function book_function() {
 
-    var book_string = "@book{";
+    var book_string = "@online{";
     var code = document.getElementById("code").value + ',\n';
-    var title = '  title     = "' + document.getElementById("title").value + '",\n';
+    var name = '  name     = "' + document.getElementById("title").value + '",\n';
     var author = '  author    = "' + document.getElementById("title").value + '",\n';
-    var year = '  year      = ' + document.getElementById("year").value + ',\n';
-    var publisher = '  publisher = "' + document.getElementById("publisher").value + '",\n';
-    var address = '  address   = "' + document.getElementById("address").value + '"\n' + "}";
+    var title = '  title      = ' + document.getElementById("year").value + ',\n';
+    var addendum = '  addendum = "' + document.getElementById("publisher").value + '",\n';
+    var keywords = '  keywords   = "' + document.getElementById("address").value + '"\n' + "}";
 
     book_string += code;
     book_string += title;
@@ -30,14 +30,14 @@ function book_function() {
 
 function book_clear() {
 
-    if (document.getElementById("book_button_clear").innerHTML == "Confirm") {
+    if (document.getElementById("online_button_clear").innerHTML == "Confirm") {
 
         document.getElementById("code").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("author").value = "";
         document.getElementById("title").value = "";
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
-        document.getElementById("publisher").value = "";
-        document.getElementById("address").value = "";
+        document.getElementById("addendum").value = "";
+        document.getElementById("keywords").value = "";
         document.getElementById("book_button_clear").style.background = '#087cfc';
         document.getElementById("book_button_clear").style.borderColor = '#087cfc';
         document.getElementById("book_button_clear").innerHTML = "Clear all";
