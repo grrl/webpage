1a4528269653b1e267c96da77c39c9344c260901
user <user@DESKTOP-09J267L>
2022-04-03 19:13:01 +0300

    uwu

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-// JavaScript source code
+﻿// JavaScript source code
 
 //activate when user presses submi
 
@@ -24,7 +24,7 @@ function book_function() {
 
     var book_string = "@book{";
     var code = document.getElementById("code").value + ',\n';
-    var name = '  title     = "' + document.getElementById("title").value + '",\n';
+    var title = '  title     = "' + document.getElementById("title").value + '",\n';
     //var author = '  author    = "' + document.getElementById("author").value + '",\n';
     var year = '  year      = ' + document.getElementById("year").value + ',\n';
     var publisher = '  publisher = "' + document.getElementById("publisher").value + '",\n';
@@ -36,6 +36,12 @@ function book_function() {
     book_string += publisher;
     book_string += address;
     navigator.clipboard.writeText(book_string);
+
+    document.getElementById("book_button").innerHTML = "　　　Copied　　";
+    document.title = "your narrative has been updated";
+    sleep(500).then(() => {
+        document.getElementById("book_button").innerHTML = "Copy to clipboard";
+    });
 }
 
 function book_clear() {
