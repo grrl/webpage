a472f89bc326905d2077d253909837038b3fbf15
user <user@DESKTOP-09J267L>
2022-04-12 23:03:38 +0300

    uwu

--- conference.js
+++ conference.js
@@ -22,8 +22,8 @@ function conference_function() {
 
     var book_string = "@InProceedings{";
     var code = document.getElementById("code").value + ',\n';
-    var title = '  title     = "' + document.getElementById("title").value + '",\n';
     var author = '  author    = "' + document.getElementById("author").value + '",\n';
+    var title = '  title     = "' + document.getElementById("title").value + '",\n';
     var booktitle = '  booktitle = "' + document.getElementById("booktitle").value + '",\n';
     var year = '  year      = ' + document.getElementById("year").value + ',\n';
     var month = '  month     = ' + document.getElementById("month").value + ',\n';
@@ -33,8 +33,8 @@ function conference_function() {
 
 
     book_string += code;
-    book_string += title;
     book_string += author;
+    book_string += title;
     book_string += booktitle;
     book_string += year;
     book_string += month;
@@ -56,11 +56,14 @@ function conference_clear() {
     if (document.getElementById("conference_button_clear").innerHTML == "Confirm") {
 
         document.getElementById("code").value = "";
-        document.getElementById("title").value = "";
         document.getElementById("author").value = "";
+        document.getElementById("title").value = "";
+        document.getElementById("booktitle").value = "";
         document.getElementById("year").value = "";
+        document.getElementById("month").value = "";
         document.getElementById("publisher").value = "";
         document.getElementById("address").value = "";
+        document.getElementById("pages").value = "";
         document.getElementById("book_button_clear").style.background = '#087cfc';
         document.getElementById("book_button_clear").style.borderColor = '#087cfc';
         document.getElementById("book_button_clear").innerHTML = "Clear all";
