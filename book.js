64aab2887d29aadea5e5936da8c43e146d9d08a8
user <user@DESKTOP-09J267L>
2022-04-13 19:27:39 +0300

    uwu

--- book.js
+++ book.js
@@ -20,15 +20,362 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 var timer;
-function rotate_title(onoff, delay) {
+var index = 0;
+function rotate_title(myindex, delay) {
     var title1 = "4 Rapid Development";
     var title2 = "Website for developers, designers and webmaster";
 
     var title = "";
 
-    var index = onoff;
+    //var index = onoff;
+    var index_string = "";
+
+    //var myindex = 0;
+
+    switch (myindex) {
+
+        case 0:
+            index_string = "Your narrative has been updated";
+            myindex = 0;
+            break;
+        case 1:
+            index_string = "our narrative has been updated ";
+            myindex = 1;
+
+            break;
+        case 2:
+            index_string = "ur narrative has been updated Y";
+            myindex = 2;
+
+            break;
+        case 3:
+            myindex = 3;
+
+            index_string = "r narrative has been updated Yo";
+            break;
+        case 4:
+            myindex = 4;
+
+            index_string = " narrative has been updated You";
+            break;
+        case 5:
+            myindex = 5;
+
+            index_string = "narrative has been updated Your";
+            break;
+        case 6:
+            myindex = 6;
+
+            index_string = "arrative has been updated Your ";
+            break;
+        case 7:
+            myindex = 7;
+
+            index_string = "rrative has been updated Your n";
+            break;
+        case 8:
+            myindex = 8;
+
+            index_string = "rative has been updated Your na";
+            break;
+        case 9:
+            myindex = 9;
+
+            index_string = "ative has been updated Your nar";
+            break;
+        case 10:
+            myindex = 10;
+
+            index_string = "tive has been updated Your narr";
+            break;
+        case 11:
+            myindex = 11;
+
+            index_string = "ive has been updated Your narra";
+            break;
+        case 12:
+            myindex = 12;
+
+            index_string = "ve has been updated Your narrat";
+            break;
+        case 13:
+            myindex = 13;
+
+            index_string = "e has been updated Your narrati";
+            break;
+        case 14:
+            myindex = 14;
+
+            index_string = " has been updated Your narrativ";
+            break;
+        case 15:
+            myindex = 15;
+
+            index_string = "has been updated Your narrative";
+            break;
+        case 16:
+            myindex = 16;
+
+            index_string = "as been updated Your narrative ";
+            break;
+        case 17:
+            myindex = 17;
+
+            index_string = "s been updated Your narrative h";
+            break;
+        case 18:
+            myindex = 18;
+
+            index_string = " been updated Your narrative ha";
+            break;
+        case 19:
+            myindex = 19;
+
+            index_string = "been updated Your narrative has";
+            break;
+        case 20:
+            myindex = 20;
+
+            index_string = "een updated Your narrative has ";
+            break;
+        case 21:
+            myindex = 21;
+
+            index_string = "en updated Your narrative has b";
+            break;
+        case 22:
+            myindex = 22;
+
+            index_string = "n updated Your narrative has be";
+            break;
+        case 23:
+            myindex = 23;
+
+            index_string = " updated Your narrative has bee";
+            break;
+        case 24:
+            myindex = 24;
+
+            index_string = "updated Your narrative has been";
+            break;
+        case 25:
+            myindex = 25;
+
+            index_string = "pdated Your narrative has been ";
+            break;
+        case 26:
+            myindex = 26;
+
+            index_string = "dated Your narrative has been u";
+            break;
+        case 27:
+            myindex = 27;
+
+            index_string = "ated Your narrative has been up";
+            break;
+        case 28:
+            myindex = 28;
+
+            index_string = "ted Your narrative has been upd";
+            break;
+        case 29:
+            myindex = 29;
+
+            index_string = "ed Your narrative has been upda";
+            break;
+        case 30:
+            myindex = 30;
+
+            index_string = "d Your narrative has been updat";
+            break;
+        case 31:
+            myindex = 31;
+
+            index_string = " Your narrative has been update";
+            break;
+        case 32:
+            myindex = 32;
+
+            index_string = "Your narrative has been updated";
+            break;
+        case 33:
+            myindex = 33;
+
+            index_string = " Your narrative has been update";
+            break;
+        case 34:
+            myindex = 34;
+
+            index_string = "d Your narrative has been updat";
+            break;
+        case 35:
+            myindex = 35;
+
+            index_string = "ed Your narrative has been upda";
+            break;
+        case 36:
+            myindex = 36;
+
+            index_string = "ted Your narrative has been upd";
+            break;
+        case 37:
+            myindex = 37;
+
+            index_string = "ated Your narrative has been up";
+            break;
+        case 38:
+            myindex = 38;
+
+            index_string = "dated Your narrative has been u";
+            break;
+        case 39:
+            myindex = 39;
+
+            index_string = "pdated Your narrative has been ";
+            break;
+        case 40:
+            myindex = 40;
+
+            index_string = "updated Your narrative has been";
+            break;
+        case 41:
+            myindex = 41;
+
+            index_string = " updated Your narrative has bee";
+            break;
+        case 42:
+            myindex = 42;
+
+            index_string = "n updated Your narrative has be";
+            break;
+        case 43:
+            myindex = 43;
+
+            index_string = "en updated Your narrative has b";
+        case 44:
+            myindex = 44;
+
+            index_string = "een updated Your narrative has ";
+            break;
+        case 45:
+            myindex = 45;
+
+            index_string = "been updated Your narrative has";
+            break;
+        case 46:
+            myindex = 46;
+
+            index_string = " been updated Your narrative ha";
+            break;
+        case 47:
+            myindex = 47;
+
+            index_string = "s been updated Your narrative h";
+            break;
+        case 48:
+            myindex = 48;
+
+            index_string = "as been updated Your narrative ";
+            break;
+        case 49:
+
+            myindex = 49;
+
+            index_string = "has been updated Your narrative";
+            break;
+        case 50:
+            myindex = 50;
+
+            index_string = " has been updated Your narrativ";
+            break;
+        case 51:
+            myindex = 51;
+
+            index_string = "e has been updated Your narrati";
+            break;
+        case 52:
+            myindex = 52;
+
+            index_string = "ve has been updated Your narrat";
+            break;
+        case 53:
+            myindex = 53;
+
+            index_string = "ive has been updated Your narra";
+            break;
+        case 54:
+
+            myindex = 54;
+
+            index_string = "tive has been updated Your narr";
+            break;
+        case 55:
+
+            myindex = 55;
+
+            index_string = "ative has been updated Your nar";
+            break;
+        case 56:
+
+            myindex = 56;
+
+            index_string = "rative has been updated Your na";
+            break;
+        case 57:
+
+            myindex = 57;
+
+            index_string = "rrative has been updated Your n";
+            break;
+        case 58:
+
+            myindex = 58;
 
 
+            index_string = "arrative has been updated Your ";
+            break;
+        case 59:
+
+            myindex = 59;
+
+            index_string = "narrative has been updated Your";
+            break;
+        case 60:
+
+            myindex = 60;
+
+            index_string = " narrative has been updated You";
+            break;
+        case 61:
+
+            myindex = 61;
+
+            index_string = "r narrative has been updated Yo";
+            break;
+        case 62:
+
+            myindex = 62;
+
+            index_string = "ur narrative has been updated Y";
+            break;
+        case 63:
+
+            myindex = 63;
+
+            index_string = "our narrative has been updated ";
+            break;
+        case 64:
+
+            myindex = -1;
+
+            index_string = "Your narrative has been updated";
+            break;
+        default:
+
+    }
+
+    myindex++;
+    /*
     if (index == 0) {
         title = title1;
         index = 1;
@@ -37,8 +384,9 @@ function rotate_title(onoff, delay) {
         title = title2;
         index = 0;
     }
-    document.title = title;
-    timer = window.setTimeout("rotate_title(" + index + "," + delay + ")", delay);
+    */
+    document.title = index_string;
+    timer = window.setTimeout("rotate_title(" + myindex + "," + delay + ")", delay);
 
 }
 
@@ -71,7 +419,7 @@ function book_function() {
     sleep(500).then(() => {
         document.getElementById("book_button").innerHTML = "Copy to clipboard";
     });
-    rotate_title(1, 1000);
+    rotate_title(0, 200);
 }
 
 //rotate_title(0, 1000);
@@ -79,7 +427,7 @@ function book_function() {
 function book_clear() {
 
     stop_timeout();
-
+    document.title = "Citations made simple";
 
     if (document.getElementById("book_button_clear").innerHTML == "Confirm") {
 
@@ -93,7 +441,6 @@ function book_clear() {
         document.getElementById("book_button_clear").style.borderColor = '#087cfc';
         document.getElementById("book_button_clear").innerHTML = "Clear all";
         //make switching
-        document.title = "Overleaf citations made simple";
 
     }
     else {
